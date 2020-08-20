e0d46af77ff41020713c0b58aa716c0d06e29b96
inc0der <inc0der.code@gmail.com>
2020-08-19 21:35:07 -0300

    Initial commit

---
+++ src/Chapters.hx
@@ -0,0 +1,14 @@
+package;
+
+import utils.Comment;
+
+class Chapters {
+ public static var Params: Dynamic;
+
+ public static function main() {
+  Comment.pluginParams("
+    @author inc0der | LunaTechsDev
+    @plugindesc Add a chapter selection system to your game <Luna_Chapters>.
+   ");
+ }
+}
