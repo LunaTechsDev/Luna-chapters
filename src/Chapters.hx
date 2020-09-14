d7280a1a23eb3e444f352af4dd250672f289c363
inc0der <inc0der.code@gmail.com>
2020-09-14 15:37:06 -0300

    Update main entry

--- src/Chapters.hx
+++ src/Chapters.hx
@@ -1,14 +1,28 @@
 package;
 
-import utils.Comment;
+import rm.managers.StorageManager;
+import overrides.DataManager;
+import pixi.interaction.EventEmitter;
+import windows.TitleCommand;
+import rm.managers.PluginManager;
+import scenes.SceneTitle;
 
 class Chapters {
  public static var Params: Dynamic;
+ public static var Emitter: EventEmitter;
+ public static var Store: ChapterStore;
 
  public static function main() {
-  Comment.pluginParams("
-    @author inc0der | LunaTechsDev
-    @plugindesc Add a chapter selection system to your game <Luna_Chapters>.
-   ");
+  var rawParams = PluginManager.parameters('Luna_Chapters');
+  Params = utils.Parse.parseParameters(rawParams);
+  Emitter = new EventEmitter();
+  Store = new ChapterStore();
+
+  Emitter.on('game-saved', (savename: String) -> {
+   StorageManager.saveObject(savename, Store);
+  });
+  DataManager.patch();
+  SceneTitle.patch();
+  TitleCommand.patch();
  }
 }
